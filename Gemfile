365630cc416e825523551e97e5069d137205ff84
EugeniaM <maltsevae88@gmail.com>
2017-07-15 23:54:34 +0300

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 gem 'pg', '~> 0.20'
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'rspec-rails'
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -10,6 +11,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
